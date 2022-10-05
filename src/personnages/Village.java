d59239f7c9585a7b7dfb576a3c74b7963bc465fa
Skira01 <jeremrichard31620@gmail.com>
2022-10-05 15:17:21 +0200

    Debut du tp2

---
+++ src/personnages/Village.java
@@ -0,0 +1,38 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+	
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	
+	
+	public Village(String nom, int nbVillageoisMaximum) {
+	this.nom = nom;
+	villageois = new Gaulois[nbVillageoisMaximum];
+	this.nbVillageois = nbVillageoisMaximum;
+	}
+	
+	public void ajouterHabitant(Gaulois g) {
+		villageois[nbVillageois] = g;
+		nbVillageois ++ ;
+	}
+	
+	public Gaulois trouverHabitant(int numero) {
+		return villageois[numero];
+	}
+	
+	public void setChef(Chef chef) {
+	this.chef = chef;
+	}
+	
+	public main(tring) {
+		village(nom = "village des irreductibles", nbVillageoisMaximum = 30)
+	}
+	
+	public String getNom() {
+	return nom;
+	}
+
+}
